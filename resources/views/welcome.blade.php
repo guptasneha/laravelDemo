a83722fc6fe99829ebf5e61c806379aec30d67e3
guptasneha <gupta.sneha@intactinnovations.com>
2016-06-27 16:18:18 +0530

    27Jun16#415
    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div class="title">Welcome</div>
             </div>
         </div>
     </body>
